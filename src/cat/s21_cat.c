4d63cdf6c7fe3f05bb0d2151cd9eb12d82141ad7
Keira Skip <keiraski@pr-h2.kzn.21-school.ru>
2024-05-03 22:06:40 +0300

    complite cat

--- src/cat/s21_cat.c
+++ src/cat/s21_cat.c
@@ -1,136 +1,136 @@
-#include <stdbool.h>
+#include <getopt.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-void flags_parser(char *flags, int argc, char **argv, int *index);
-void append_flags(char *flags, char flag);
-void append_flag(char *flags, char flag);
-int print_file(char *name, char *flags);
-void print_symb(int c, int *prev, char *flags, int *index, bool *eline_printed);
+typedef struct Arguments {
+  int b, n, s, E, T, v;
+} arguments;
 
-int main(int argc, char **argv) {
-  if (argc < 2) {
-    printf("Usage: %s <filename>n", argv[0]);
-    return 1;
-  }
-  char flags[7] = "0";  // массив флагов
-  int index_end_flags = 1;
-  flags_parser(flags, argc, argv, &index_end_flags);
-
-  int err_code = 0;
-  // Перебираем все аргументы после флагов, считая их именами файлов
-  for (int i = index_end_flags; i < argc; i++) {
-    // Печатаем содержимое каждого файла
-    if (print_file(argv[i], flags) != 0) {
-      fprintf(stderr, "Error printing file: %s\n", argv[i]);
-      err_code = 1;
+arguments argument_parser(int argc, char** argv) {
+  arguments arg = {0, 0, 0, 0, 0, 0};
+  struct option long_options[] = {{"number", no_argument, NULL, 'n'},
+                                  {"number-nonblank", no_argument, NULL, 'b'},
+                                  {"squeeze-blank", no_argument, NULL, 's'},
+                                  {0, 0, 0, 0}};
+  int opt;
+  while ((opt = getopt_long(argc, argv, "bnEeTtsv", long_options, NULL)) !=
+         -1) {
+    switch (opt) {
+      case 'b':
+        arg.b = 1;
+        break;
+      case 'n':
+        arg.n = 1;
+        break;
+      case 's':
+        arg.s = 1;
+        break;
+      case 'e':
+        arg.E = 1;
+        arg.v = 1;
+        break;
+      case 'E':
+        arg.E = 1;
+        break;
+      case 't':
+        arg.T = 1;
+        arg.v = 1;
+        break;
+      case 'T':
+        arg.T = 1;
+        break;
+      case 'v':
+        arg.v = 1;
+        break;
+      case '?':
+        fprintf(stderr, "Usage: %s [-bnEeTtsv] [file...]\n", argv[0]);
+        exit(EXIT_FAILURE);
+      default:
+        break;
     }
   }
-  return err_code;
+  return arg;
 }
 
-void flags_parser(char *flags, int argc, char **argv, int *index) {
-  // Пройдемся по всем аргументам, кроме первого (имени программы)
-  for (int i = 1; i < argc; i++) {
-    // Если аргумент начинается не на '-' или является строкой "-" или "--"
-    // (сигнал, что опции кончились), считаем, что флаги опций кончились
-    if (argv[i][0] != '-' || strcmp(argv[i], "--") == 0 ||
-        strcmp(argv[i], "-") == 0) {
-      *index = i;
-      return;  // Выходим из функции, так как флаги закончились
-    } else {
-      // Флаги могут быть написаны слитно, например: -bE
-      // Поэтому проходимся по каждому символу аргумента, кроме первого
-      for (size_t j = 1; j < strlen(argv[i]); j++) {
-        append_flags(flags, argv[i][j]);
+char v_output(char ch) {
+  if (ch == '\n' || ch == '\t') {
+    return ch;
+  }
+  if (ch >= 0 && ch <= 31) {
+    putchar('^');
+    return ch + 64;
+  } else if (ch == 127) {
+    putchar('^');
+    return '?';
+  }
+  return ch;
+}
+
+void outline(arguments* arg, char* line, ssize_t read, int* line_count,
+             int* empty_count) {
+  int empty_line = read == 1 && line[0] == '\n';
+  if (empty_line) {
+    if (arg->s) {
+      (*empty_count)++;
+      if (*empty_count > 1) {
+        return;
       }
     }
-  }
-  *index =
-      argc;  // Если флаги не найдены, устанавливаем index в конец массива argv
-}
-
-struct s_avi_flags {
-  char flag;               // флаг
-  char *equivalent_flags;  // эквивалентные простые флаги
-};
-
-void append_flags(char *flags, char flag) {
-  // доступные флаги
-  struct s_avi_flags avi_flags[8] = {{'b', "b"},  {'E', "E"}, {'e', "Ev"},
-                                     {'n', "n"},  {'s', "s"}, {'T', "T"},
-                                     {'t', "Tv"}, {'v', "v"}};
-
-  for (int i = 0; i < 8; i++) {
-    if (avi_flags[i].flag == flag) {
-      for (size_t j = 0; j < strlen(avi_flags[i].equivalent_flags); j++) {
-        append_flag(flags, avi_flags[i].equivalent_flags[j]);
-      }
-      break;
-    }
-  }
-}
-
-// добавить 1 флаг в массив флагов
-void append_flag(char *flags, char flag) {
-  if (strchr(flags, flag) == NULL) {  // если такого флага нет
-    size_t len = strlen(flags);
-    flags[len] = flag;  // добавляем флаг
-    flags[len + 1] = '\0';  // обновляем нулевой терминатор
-  }
-}
-
-int print_file(char *name, char *flags) {
-  int err_code = 0;
-  FILE *f =
-      fopen(name, "rt");  // Попытка открыть файл для чтения в текстовом режиме
-
-  if (f != NULL) {
-    int index = 0;
-    bool eline_printed = false;
-    int c = fgetc(f), prev = '\n';
-    while (c != EOF) {
-      print_symb(c, &prev, flags, &index, &eline_printed);
-      c = fgetc(f);
-    }
-    fclose(f);
   } else {
-    err_code = 1;
-    // printf("Файла не существует");
+    *empty_count = 0;
   }
-  return err_code;
+
+  if ((arg->n && !arg->b) || (arg->b && !empty_line)) {
+    printf("%6d\t", (*line_count)++);
+  }
+
+  for (ssize_t i = 0; i < read; i++) {
+    if (arg->v && line[i] != '\n' && line[i] != '\t' &&
+        (line[i] < 32 || line[i] == 127)) {
+      line[i] = v_output(line[i]);
+    } else if (arg->E && line[i] == '\n') {
+      // putchar('$');
+      if (arg->b && line[0] == '\n') {
+        printf("      \t$");
+      } else
+        putchar('$');
+    } else if (arg->T && line[i] == '\t') {
+      printf("^I");
+      continue;
+    } 
+    putchar(line[i]);
+  }
 }
 
-void print_symb(int c, int *prev, char *flags, int *index,
-                bool *eline_printed) {
-  // если s и это пустая строка и пустая строка уже была выведена, пропустим,
-  // сюда не зайдем
-  if (!(strchr(flags, 's') != NULL && *prev == '\n' && c == '\n' &&
-        *eline_printed)) {
-    if (*prev == '\n' && c == '\n')
-      *eline_printed = true;
-    else
-      *eline_printed = false;
+void output(arguments* arg, FILE* f) {
+  char* line = NULL;
+  size_t memline = 0;
+  ssize_t read = 0;
+  int line_count = 1;
+  int empty_count = 0;
 
-    // если ( (n без b) или (b и текущий символ не '\n') ) и пред символ '\n'
-    if (((strchr(flags, 'n') != NULL && strchr(flags, 'b') == NULL) ||
-         (strchr(flags, 'b') != NULL && c != '\n')) &&
-        *prev == '\n') {
-      *index += 1;
-      printf("%6d\t", *index);
-    }
-
-    if (strchr(flags, 'E') != NULL && c == '\n') printf("$");
-    if (strchr(flags, 'T') != NULL && c == '\t') {
-      printf("^");
-      c = '\t' + 64;
-    }
-    if (strchr(flags, 'v') != NULL && c >= 0 && c <= 31 && c != '\n' &&
-        c != '\t') {
-      printf("^");
-      c = c + 64;
-    }
-    fputc(c, stdout);
+  while ((read = getline(&line, &memline, f)) != -1) {
+    outline(arg, line, read, &line_count, &empty_count);
   }
-  *prev = c;
+
+  free(line);
+}
+
+int main(int argc, char* argv[]) {
+  arguments arg = argument_parser(argc, argv);
+  if (optind == argc) {
+    output(&arg, stdin);
+  } else {
+    for (int i = optind; i < argc; i++) {
+      FILE* f = fopen(argv[i], "r");
+      if (!f) {
+        perror(argv[i]);
+        continue;
+      }
+      output(&arg, f);
+      fclose(f);
+    }
+  }
+  return 0;
 }
